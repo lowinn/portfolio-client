c73d727ba09b7c976fa00fca931d1fd52569c1a0
lowin <longsanawinkh@gmail.com>
2024-05-12 11:49:24 +0700

    update

--- src/app/[locale]/page/about/page.tsx
+++ src/app/[locale]/page/about/page.tsx
@@ -2,6 +2,9 @@ import React from 'react'
 
 export default function page() {  
   return (
-    <div className='text-8xl'>Welcome to about page!</div>
+
+    <div className='text-8xl'>Welcome to about page!
+      <p>Lorem ipsum dolor sit amet, consectetur adipisicing.</p>
+    </div>
   )
 }
--- src/app/[locale]/page/blog/page.tsx
+++ src/app/[locale]/page/blog/page.tsx
@@ -4,7 +4,7 @@ import React from 'react'
 export default function page() {
   return (
     <div>
-        <h1>Blog</h1>
+        <h1>Blog 2024</h1>
         <p>This is the blog page.</p>
     </div>
   )

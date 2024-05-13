56a26be80203febc9cb4971d16413fbe6dcbe4ef
leakena <naahshii@gmail.com>
2024-05-13 23:34:51 +0700

    contant in about page

--- src/app/[locale]/page/about/page.tsx
+++ src/app/[locale]/page/about/page.tsx
@@ -1,10 +1,59 @@
 import React from 'react'
+import Image from 'next/image'
+import { FaInstagram } from "react-icons/fa";
+import { MdSportsBasketball } from "react-icons/md";
+import { BsTwitterX } from "react-icons/bs";
 
 export default function page() {  
   return (
+  
+    <div className=''>
 
-    <div className='text-8xl'>Welcome to about page!
-      <p>Lorem ipsum dolor sit amet, consectetur adipisicing.</p>
-    </div>
+
+      <div className=' w-full h-[500px]  flex'>
+
+
+        <div className='w-[550px] h-[400px] ml-32'>
+        <Image 
+    src="/image.png"  
+    alt="GFG logo imported from public directory" 
+    className=' rounded-full mt-24' 
+    width={50}  
+    height={10} 
+/>
+        
+        <h5 className=' mt-4'>Hello I'm Lowin </h5>
+
+        </div>
+        <div className='w-[550px] h-[400px]'>
+          <h5 className=' mt-28'>A website designer in Indonesia</h5>
+          <p className='text-gray-700'>Passionate Creating Experience for designer!</p>
+
+          <button className='border  w-32 h-9 rounded-xl  bg-gray-950 mt-7 text-white p-1'>Talk with me </button>
+          <button className='border  w-32 h-9 rounded-xl ml-8 p-1'>See my work </button>
+        </div>
+
+
+
+        <div className=' w-11 h-20 p-14'>
+        <FaInstagram  className=' w-12 h-9'/>
+        <MdSportsBasketball className=' w-12 h-9 mt-6' />
+        <BsTwitterX className=' w-12 h-9 mt-6'/>
+        
+
+        </div>
+
+     
+        </div>
+        
+      
+
+
+
+
+      </div>
+  
+
+ 
   )
 }
